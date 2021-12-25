93dc2f005fc9793701630c0b9e4151a0948edbc0
DESKTOP-GRMUN4M\Umar <hamids1193@gmail.com>
2021-12-25 15:49:12 +0530

    interface demo

---
+++ C-Sharp-Tutorials/C-Sharp-Tutorials/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Tutorials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFile file1 = new File();
+
+            File file2 = new File();
+
+            file1.ReadFile();
+
+            file1.WriteFile("Hello");
+
+            file2.ReadFile();
+
+            Console.ReadKey();
+        }   
+    }
+
+    class File : IFile
+    {
+        public void ReadFile()
+        {
+            Console.WriteLine("Read file");
+        }
+
+        public void WriteFile(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface IFile
+    {
+        void ReadFile();
+        void WriteFile(string text);
+    }
+}
