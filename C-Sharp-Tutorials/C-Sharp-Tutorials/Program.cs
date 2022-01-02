dd5a876cb9e50793cb19499e7625af879bbe7b90
DESKTOP-GRMUN4M\Umar <hamids1193@gmail.com>
2022-01-02 15:02:16 +0530

    List Collection

--- C-Sharp-Tutorials/C-Sharp-Tutorials/Program.cs
+++ C-Sharp-Tutorials/C-Sharp-Tutorials/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_Sharp_Tutorials
 {
@@ -10,58 +9,123 @@ namespace C_Sharp_Tutorials
     {
         static void Main(string[] args)
         {
-            //Employee employee = new Employee(10, 20);
-            //employee.Print();
-            //employee.Print(100);
+            List<Employee> employees = new List<Employee>();
 
-            ////Employee employee2 = new Employee();
+            employees.Add(new Employee() { 
+                Id = 1,
+                Name = "Umar",
+            });
 
-            //Console.WriteLine(Employee.num3);
-            //Console.WriteLine(Employee.num4);
+            employees.Add(new Employee()
+            {
+                Id = 3,
+                Name = "Hamid",
+            });
 
-            //Customer customer = new Customer();
+            employees.Add(new Employee()
+            {
+                Id = 1,
+                Name = "Wasim",
+            });
 
-            CopyConstructorDemo copyConstructor = new CopyConstructorDemo(1, "Umar");
-            Console.WriteLine($"Id = {copyConstructor.Id} & Name = {copyConstructor.Name}");
+            foreach (Employee e in employees.OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"Id = {e.Id} Name = {e.Name}");
+            }
 
-            //Copy contructor call
-            CopyConstructorDemo copyConstructor1 = new CopyConstructorDemo(copyConstructor);
-            Console.WriteLine($"Id = {copyConstructor1.Id} & Name = {copyConstructor1.Name}");
+            Console.WriteLine("-----------------------------------------------------------");
 
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(1);
+            arrayList.Add("Umar");
+
+            foreach (var e in arrayList)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            Stack<int> s = new Stack<int>();
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+
+            foreach (var e in s)
+            {
+                Console.WriteLine(e);
+            }
+
+            Queue<int> s1 = new Queue<int>();
+            s1.Enqueue(1);
+            s1.Enqueue(2);
+            s1.Enqueue(3);
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            foreach (var e in s1)
+            {
+                Console.WriteLine(e);
+            }
+
+            Queue s2 = new Queue();
+            s2.Enqueue(1);
+            s2.Enqueue("Umar");
+            s2.Enqueue(3);
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            foreach (var e in s2)
+            {
+                Console.WriteLine(e);
+            }
+
+            Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+
+            keyValuePairs.Add(1, "Umar");
+            keyValuePairs.Add(3, "Hamid");
+            keyValuePairs.Add(2, "Wasim");
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            foreach(var e in keyValuePairs)
+            {
+                Console.WriteLine($"key = {e.Key}, Value = {e.Value}");
+            }
+
+            SortedDictionary<int, string> keyValuePairs1 = new SortedDictionary<int, string>();
+
+            keyValuePairs1.Add(1, "Umar");
+            keyValuePairs1.Add(3, "Hamid");
+            keyValuePairs1.Add(2, "Wasim");
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            foreach (var e in keyValuePairs1)
+            {
+                Console.WriteLine($"key = {e.Key}, Value = {e.Value}");
+            }
+
+            HashSet<int> vs = new HashSet<int>();
+            vs.Add(1);
+            vs.Add(2);
+            vs.Add(2);
+
+            Console.WriteLine("-----------------------------------------------------------");
+
+            foreach (var e in vs)
+            {
+                Console.WriteLine(e);
+            }
 
             Console.ReadKey();
         }
-
     }
 
-    public class Employee
+    class Employee
     {
-        int num1, num2;
-        public static int num3, num4;
-        static Employee()
-        {
-            Console.WriteLine("Default constructor");
-        }
-
-        public Employee(int a, int b)
-        {
-            num1 = a;
-            num2 = b;
-        }
-
-        public void Print()
-        {
-            Console.WriteLine($"num1 = {num1} & num2 = {num2}");
-        }
-
-        public void Print(int a)
-        {
-            Console.WriteLine(a);
-        }
-
-        public void Print(int a, int b)
-        {
-            Console.WriteLine(a);
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
